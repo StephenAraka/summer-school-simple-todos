74175e25dfb245f255b2643b074480cf3c2ab04f
StevenAraka <stevenaraka@gmail.com>
2018-06-18 10:37:52 +0300

    new commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require("./main.css")
+require("./bootstrap/css/bootstrap.min.css")
 let app = document.querySelector('#app')
-
-app.innerHTML = '<h2>Welcome to summer-school-simple-todos</h2>'
+app.innerHTML = "<div id='banner'> <h3> TO DO APP </h3> <p> The date today is: </p> <div id='button'> <button><span class = 'glyphicon'> &#x2b;</span></button> <button><span class = 'glyphicon'> &#xe086;</span></button> </div> </div>";

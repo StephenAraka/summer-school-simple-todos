ab7469b8303d6d4471d060de18d7662c01e25278
StevenAraka <stevenaraka@gmail.com>
2018-06-19 13:04:56 +0300

    Nigeria will win world cup

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ require("./main.css");
 require("./bootstrap/css/bootstrap.min.css");
 
 let app = document.querySelector('#app')
-    //app.innerHTML = "";
-    //app.innerHTML += "<div id='list'> <h5>Today's Schedule</h5> "
-    //app.innerHTML += "<table><tr><td> <button class='btn btn-primary'><span class='badge'>active</span></button> </td> <td> Go to work</td> <td>8:00 - 16:00</td> </tr> <tr><td> <button class='btn btn-primary'><span class='badge'>pending</span></button> </td> <td> Attend accounts meeting </td> <td>10:00 - 12:00</td> </tr> <tr><td> <button class='btn btn-primary'><span class='badge'>canceled</span></button> </td> <td> Watch WorldCup </td> <td>13:00 - 15:00</td> </tr>  <tr><td> <button class='btn btn-primary'><span class='badge'>active</span></button> </td> <td> Watch WorldCup </td> <td>13:00 - 15:00</td> </tr> </table></div> "
-    //app.innerHTML += "<tr><td> <button>badge</button> </td> <td> Watch WorldCup </td> <td>13:00 - 15:00</td> </tr>"
-    //app.innerHTML += "<tr><td> <button>badge</button> </td> <td> pick kids from school</td> <td>16:300 - 18:00</td> </tr>"
-    //app.innerHTML += "</table></div>"
+
+
+//app.innerHTML = "";
+//app.innerHTML += "<div id='list'> <h5>Today's Schedule</h5> "
+//app.innerHTML += "<table><tr><td> <button class='btn btn-primary'><span class='badge'>active</span></button> </td> <td> Go to work</td> <td>8:00 - 16:00</td> </tr> <tr><td> <button class='btn btn-primary'><span class='badge'>pending</span></button> </td> <td> Attend accounts meeting </td> <td>10:00 - 12:00</td> </tr> <tr><td> <button class='btn btn-primary'><span class='badge'>canceled</span></button> </td> <td> Watch WorldCup </td> <td>13:00 - 15:00</td> </tr>  <tr><td> <button class='btn btn-primary'><span class='badge'>active</span></button> </td> <td> Watch WorldCup </td> <td>13:00 - 15:00</td> </tr> </table></div> "
+
+//app.innerHTML += "<tr><td> <button>badge</button> </td> <td> Watch WorldCup </td> <td>13:00 - 15:00</td> </tr>"
+//app.innerHTML += "<tr><td> <button>badge</button> </td> <td> pick kids from school</td> <td>16:300 - 18:00</td> </tr>"
+//app.innerHTML += "</table></div>"
 
 app.innerHTML += "";

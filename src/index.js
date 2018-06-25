2d74984e1f8a732595a4a8db5bb873b7bae0ddb0
ObothA <khyper234@gmail.com>
2018-06-25 00:02:10 -0700

    purpose: to pull changes

--- src/index.js
+++ src/index.js
@@ -1,46 +1,52 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("./main.css");
-class ToDoApp {
-    constructor() {
-        this.tasksArray = [];
-        this.date = new Date().toString();
-        const databutton = document.getElementById("push_data");
-        if (databutton) {
-            databutton.addEventListener("click", (eventObject) => { this.pickData(eventObject); });
-        }
-    }
-    addDate() {
-            // add date to banner
-            this.date = this.date.slice(0, 15);
-            const Pdate = document.getElementById("date");
-            if (Pdate) {
-                Pdate.innerHTML += this.date;
-            }
-        }
-        // pick data from html to array
-    pickData(event) {
-        const table = document.getElementById("tasksTable");
-        event.preventDefault();
-        const inputTaskName = document.getElementById("taskName").value;
-        const inputTime = document.getElementById("taskTime").value;
-        // create object
-        const obj = {
-            status: "active",
-            name: inputTaskName,
-            time: inputTime,
-        };
-        this.tasksArray.push(obj);
-        let cellPosition = 0;
-        if (table) {
-            const tableRow = table.insertRow(this.tasksArray.length - 1);
-            tableRow.insertCell(cellPosition).innerHTML = `<button class='btn btn-success btn-sm'><span class='badge'>${this.tasksArray[this.tasksArray.length - 1].status}</span></button>`;
-            cellPosition += 1;
-            tableRow.insertCell(cellPosition).innerHTML = this.tasksArray[this.tasksArray.length - 1].name;
-            cellPosition += 1;
-            tableRow.insertCell(cellPosition).innerHTML = this.tasksArray[this.tasksArray.length - 1].time;
-        }
-    }
+require("./bootstrap/css/bootstrap.min.css");
+
+// add date to banner
+let date = new Date().toString();
+date = date.slice(0, 15);
+// date = `${date}`;
+
+const Pdate = document.getElementById("date");
+Pdate.innerHTML += date;
+
+
+const table = document.getElementById("tasksTable");
+
+// array of objects
+const tasksArray = [];
+
+// pick data from html to array
+function pickData(event) {
+  event.preventDefault();
+  const inputTaskName = document.getElementById("taskName").value;
+  const inputTime = document.getElementById("taskTime").value;
+
+  // create object
+  const obj = {
+    status: "active",
+    name: inputTaskName,
+    time: inputTime,
+  };
+
+  tasksArray.push(obj);
+
+  let cellPosition = 0;
+  const tableRow = table.insertRow(tasksArray.length - 1);
+  tableRow.insertCell(cellPosition).innerHTML = `<button class='btn btn-success btn-sm'><span class='badge'>${tasksArray[tasksArray.length - 1].status}</span></button>`;
+
+  cellPosition += 1;
+  tableRow.insertCell(cellPosition).innerHTML = tasksArray[tasksArray.length - 1].name;
+
+  cellPosition += 1;
+  tableRow.insertCell(cellPosition).innerHTML = tasksArray[tasksArray.length - 1].time;
+}
+
+
+const databutton = document.getElementById("push_data");
+if (databutton) {
+  databutton.addEventListener("click", pickData);
 }
 const toDoApp = new ToDoApp();
 toDoApp.addDate();

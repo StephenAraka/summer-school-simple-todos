b4b3d36193027b0c7fd6e872feec99c040220ec2
StevenAraka <stevenaraka@gmail.com>
2018-06-25 10:08:55 +0300

    need to save this before changing to class

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ require("./main.css");
 require("./bootstrap/css/bootstrap.min.css");
 
 // add date to banner
-let date = new Date().toString();
-date = date.slice(0, 15);
-date = `${date}`;
+function mydate() {
+    let date = new Date().toString();
+    return setInterval(date.slice(0, 24), 50);
+}
 
 const Pdate = document.getElementById("date");
-Pdate.innerHTML += date;
+Pdate.innerHTML += mydate();
 
+// if (Pdate) onload = function() {
+//     setInterval("Pdate.innerHTML += new Date().toLocaleString()", 50)
+// }
 
 const table = document.getElementById("tasksTable");
 
@@ -17,32 +21,32 @@ const tasksArray = [];
 
 // pick data from html to array
 function pickData(event) {
-  event.preventDefault();
-  const inputTaskName = document.getElementById("taskName").value;
-  const inputTime = document.getElementById("taskTime").value;
+    event.preventDefault();
+    const inputTaskName = document.getElementById("taskName").value;
+    const inputTime = document.getElementById("taskTime").value;
 
-  // create object
-  const obj = {
-    status: "active",
-    name: inputTaskName,
-    time: inputTime,
-  };
+    // create object
+    const obj = {
+        status: "active",
+        name: inputTaskName,
+        time: inputTime,
+    };
 
-  tasksArray.push(obj);
+    tasksArray.push(obj);
 
-  let cellPosition = 0;
-  const tableRow = table.insertRow(tasksArray.length - 1);
-  tableRow.insertCell(cellPosition).innerHTML = `<button class='btn btn-success btn-sm'><span class='badge'>${tasksArray[tasksArray.length - 1].status}</span></button>`;
+    let cellPosition = 0;
+    const tableRow = table.insertRow(tasksArray.length - 1);
+    tableRow.insertCell(cellPosition).innerHTML = `<button class='btn btn-success btn-sm'><span class='badge'>${tasksArray[tasksArray.length - 1].status}</span></button>`;
 
-  cellPosition += 1;
-  tableRow.insertCell(cellPosition).innerHTML = tasksArray[tasksArray.length - 1].name;
+    cellPosition += 1;
+    tableRow.insertCell(cellPosition).innerHTML = tasksArray[tasksArray.length - 1].name;
 
-  cellPosition += 1;
-  tableRow.insertCell(cellPosition).innerHTML = tasksArray[tasksArray.length - 1].time;
+    cellPosition += 1;
+    tableRow.insertCell(cellPosition).innerHTML = tasksArray[tasksArray.length - 1].time;
 }
 
 
 const databutton = document.getElementById("push_data");
 if (databutton) {
-  databutton.addEventListener("click", pickData);
+    databutton.addEventListener("click", pickData);
 }
